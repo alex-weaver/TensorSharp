fd2476991ed765f773a6966246e1ea00e4ee2544
Oceania2018 <haiping008@gmail.com>
2019-03-29 05:18:07 -0500

    Upgrade to .NET Standard 2.0 #7

--- src/TensorSharp/Core/MemoryCopier.cs
+++ src/TensorSharp/Core/MemoryCopier.cs
@@ -8,34 +8,9 @@ namespace TensorSharp.Core
 {
     internal static class MemoryCopier
     {
-        private static class NativeMethods32
+        public static unsafe void Copy(IntPtr destination, IntPtr source, ulong length)
         {
-            [DllImport("kernel32.dll")]
-            public static extern void CopyMemory(IntPtr destination, IntPtr source, uint length);
-        }
-
-        private static class NativeMethods64
-        {
-            [DllImport("kernel32")]
-            public static extern void RtlCopyMemory(IntPtr destination, IntPtr source, ulong length);
-        }
-
-        public static void Copy(IntPtr destination, IntPtr source, ulong length)
-        {
-            var is32 = IntPtr.Size == 4;
-
-            if (is32)
-            {
-                // Note: if this is run, length should always be in range of a uint
-                // (it should be impossible to allocate a buffer bigger than that range
-                // on a 32-bit system)
-                NativeMethods32.CopyMemory(destination, source, (uint)length);
-            }
-            else
-            {
-                NativeMethods64.RtlCopyMemory(destination, source, (ulong)length);
-            }
-
+            Buffer.MemoryCopy(source.ToPointer(), destination.ToPointer(), length, length);
         }
     }
 }
